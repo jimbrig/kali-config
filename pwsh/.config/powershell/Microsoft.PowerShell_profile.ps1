fc045be974e798080e4c7d21c2d801d71e2095f6
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2021-12-23 19:35:16 -0500

    feat: add NuGet and Powershell profiles/configs

---
+++ pwsh/.config/powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,6 @@
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+
+Set-PoshPrompt space
+
+Set-PSReadLineOption -PredictionSource History -WarningAction SilentlyContinue -ErrorAction SilentlyContinue
+Set-PSReadLineOption -PredictionViewStyle ListView -WarningAction SilentlyContinue -ErrorAction SilentlyContinue
